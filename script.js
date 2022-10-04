c1373060dc23789b55c8ed227f9326fcfbd8d632
Tigro <easterlingme95@gmail.com>
2022-10-03 22:30:12 -0500

    added prompts for lower and uppercase criteria

--- script.js
+++ script.js
@@ -12,3 +12,18 @@ function generatePassword() {
     window.alert("please choose a minumum of 8 char. and max of 128 char.");
     return;
   }
+    //prompt for whether they want or dont want lowercase letters
+  
+    var lowercase = window.confirm("Would you like lowercase letters.");
+  
+    if (lowercase === true) {
+      var beta = "abcdefghijklmnopqrstuvwxyz";
+      characters += beta;     
+    }
+    // prompt whether they want or dont want uppercase letters
+    var uppercase = window.confirm("Would you like uppercase letters.");
+    
+    if (uppercase === true) {
+      var alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      characters += alpha;      
+    }
